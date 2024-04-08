a263a919e4ebf8df7d674f6df2cbf4cf93d5b7c0
Manuel Borrero <borreromanuel95@gmail.com>
2024-04-08 19:12:53 +0200

    fix code

--- script/script.js
+++
@@ -1,7 +0,0 @@
-let cntHidden = document.querySelectorAll(".cnt-hidde");
-
-document.addEventListener('scroll', function () {
-    cntHidden.forEach(element => {
-        element.classList.add('visible');        
-    });
-})
